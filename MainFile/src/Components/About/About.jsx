a3c446dc2b2fbdc830623bf97187fb10142d2505
Baba Fakruddin <fakruddinbaba560@gmail.com>
2023-09-07 22:16:41 +0530

    added tailwind css and create a responsive navbar

--- MainFile/src/Components/About/About.jsx
+++ MainFile/src/Components/About/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function About() {
   return (
     <div>
-      <h1>THIS IS THE ABOUT PAGE</h1>
+      <h1 className='bg-red-300'>THIS IS THE ABOUT PAGE</h1>
     </div>
   )
 }
--- MainFile/src/Components/Navbar.jsx
+++ MainFile/src/Components/Navbar.jsx
@@ -1,37 +1,74 @@
-import React from 'react'
+import { useState } from "react";
+
 import "./Navbar.css"
 import Logo from"../Images/vydya-removebg-preview.png"
 import { NavLink } from 'react-router-dom'
 
+
+
 export default function Navbar() {
+  let [Menu, setMenu] = useState(false)//Checking if the menu icon is clicked or not
+
+    let menuClick = () => {//Changing the menu icon based on the visibility of main options when the web site is in phone
+        const options = document.querySelector(".OptionsList")
+        options.style.display === "flex" ? options.style.display = "none" : options.style.display = "flex"
+        options.style.display === "flex" ? setMenu(true) : setMenu(false)
+      }
+      
+      function handleResize() {
+  const navbar = document.querySelector(".Options")
+  const screenWidth = window.innerWidth;
+
+  if (screenWidth <= 1450) {
+  
+  } else {
+    navbar.style.display = 'block'; // Ensure the navbar is always visible above 550 pixels
+    
+  }
+}
+
+// Attach the event listener to the window
+window.addEventListener('resize', handleResize);
+
   return (
     <div>
      <nav>
-        <div><img className="Logo" src={Logo} alt="LOGO HERE" /></div>
+        <div><NavLink to="." end><img className="Logo" src={Logo} alt="LOGO HERE" /></NavLink></div>
         <div className='Options'>
-            <ul>
-            <li><NavLink  to="." > Home</NavLink></li>
-            <li><NavLink  to="/bloodservices" > Blood Services</NavLink></li>
-            <li><NavLink  to="/appointment" > Appointment</NavLink></li>
-            <li><NavLink  to="/userinfo" > User Info</NavLink></li>
-            <li><NavLink  to="/ourproducts" > Our Products</NavLink></li>
-            <li><NavLink  to="/about" > About</NavLink></li>
-        </ul></div>
+            <ul className="OptionsList">
+            <li><NavLink end className={({isActive})=> isActive?"activeNav":null}  to="." > Home</NavLink></li>
+            <li><NavLink className={({isActive})=> isActive?"activeNav":null}  to="/bloodservices" > Blood Services</NavLink></li>
+            <li><NavLink className={({isActive})=> isActive?"activeNav":null}  to="/appointment" > Appointment</NavLink></li>
+            <li><NavLink className={({isActive})=> isActive?"activeNav":null}  to="/userinfo" > User Info</NavLink></li>
+            <li><NavLink className={({isActive})=> isActive?"activeNav":null}  to="/ourproducts" > Our Products</NavLink></li>
+            <li><NavLink className={({isActive})=> isActive?"activeNav":null}  to="/about" > About</NavLink></li>
+        </ul>
+    
+      </div>
         <div className='loginwrapper' >
         <button>
     Sign up
-    <div class="arrow-wrapper">
-        <div class="arrow"></div>
+    <div className="arrow-wrapper">
+        <div className="arrow"></div>
 
     </div>
 </button><button>
     Login
-    <div class="arrow-wrapper">
-        <div class="arrow"></div>
+    <div className="arrow-wrapper">
+        <div className="arrow"></div>
 
     </div>
 </button>
         </div>
+        <span className="close_open"  onClick={() => {
+            menuClick();
+          }}>
+            <button >
+            {
+              Menu ? <i className="fa fa-times" aria-hidden="true"></i> : <i className="fa fa-bars" aria-hidden="true"></i>
+            }
+            </button>
+          </span>
      </nav>
     </div>
   )
